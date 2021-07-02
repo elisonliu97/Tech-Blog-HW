64ec8680c32968b4434fd2f31c511712a50dbdf4
Elison Liu <elisonliu97@gmail.com>
2021-07-02 18:58:10 -0400

    created navbar and homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,5 +2,28 @@ const router = require('express').Router();
 const { Posting, User } = require('../models');
 // const withAuth = require('../utils/auth');
 
+router.get('/', async (req, res) => {
+    try {
+
+        const postingData = await Posting.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+
+        const postings = postingData.map((posting) => posting.get({ plain: true }));
+        console.log(postings)
+
+        res.render('homepage', {
+            postings,
+            // logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
