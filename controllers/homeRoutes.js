a7d276706defc7b95a96e117ad13d0337dead8d0
Elison Liu <elisonliu97@gmail.com>
2021-07-15 15:16:24 -0400

    home route and post route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -16,10 +16,38 @@ router.get('/', async (req, res) => {
         });
 
         const postings = postingData.map((posting) => posting.get({ plain: true }));
-        console.log
 
         res.render('homepage', {
             postings,
+            // logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// specific post route
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postingData = await Posting.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+        console.log(postingData)
+        if (!postingData) {
+            console.log("no posting data")
+            return
+        }
+
+        const posting = postingData.get({ plain: true})
+        console.log(posting)
+
+        res.render('post', {
+            posting,
             // logged_in: req.session.logged_in
         });
     } catch (err) {
